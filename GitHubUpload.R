1b880e37b07ebbbe8572bff8e0cbe1be0f887f2b
Alan Bergland <aob2x@virginia.edu>
2022-12-15 15:16:32 -0500

    Update GitHubUpload.R
    linear models

--- GitHubUpload.R
+++ GitHubUpload.R
@@ -27,21 +27,21 @@ Phenowide <- readRDS("/Users/dgg/Desktop/wideform.phenotypedata (1).RDS") #201 r
 AllPhenoNoRalIdWide <- subset(Phenowide, select = -c(ral_id))
 w <- as.vector(colSums(is.na(AllPhenoNoRalIdWide)) < 66) # 50% = 100 rows; 33% = 66 rows
 Phenonarrow <- AllPhenoNoRalIdWide[, ..w]
-AllPhenonarrow <- Phenonarrow %>% 
-  mutate_if(is.numeric, ~replace_na(.,mean(., na.rm = TRUE))) 
+AllPhenonarrow <- Phenonarrow %>%
+  mutate_if(is.numeric, ~replace_na(.,mean(., na.rm = TRUE)))
 
 STwide <-fread("/Users/dgg/Desktop/ST-Lines.csv") #231 columns, 158 rows; threshold for column inclusion is 79 (50%) non-NA values in rows
 STwide <- subset(STwide, select = -c(V232))
 x <- as.vector(colSums(is.na(STwide)) < 52) #50% = 79 rows, 30% = 52 rows
 STnarrow <- STwide[, ..x]
-STData <- STnarrow %>% 
+STData <- STnarrow %>%
   mutate_if(is.numeric, ~replace_na(.,mean(., na.rm = TRUE))) #maintained 100 columns
 
 Mixedwide <- fread("/Users/dgg/Desktop/INVST-Lines.csv") #231 columns, 25 rows; threshold for column inclusion is 13 (~50%) non-NA values in rows
 Mixedwide <- subset(Mixedwide, select = -c(V232))
 y <- as.vector(colSums(is.na(Mixedwide)) < 13) #50% = 13 rows
 STnarrow <- STwide[, ..y]
-MixedData <- STnarrow %>% 
+MixedData <- STnarrow %>%
   mutate_if(is.numeric, ~replace_na(.,mean(., na.rm = TRUE))) #maintained 213 columns
 
 
@@ -49,7 +49,7 @@ INVwide <- fread("/Users/dgg/Desktop/INV-Lines.csv") #231 columns, 18 rows; thre
 INVwide <- subset(INVwide, select = -c(V232))
 z <- as.vector(colSums(is.na(INVwide)) < 6)
 INVnarrow <- INVwide[, ..z]
-INVData <- INVnarrow %>% 
+INVData <- INVnarrow %>%
   mutate_if(is.numeric, ~replace_na(.,mean(., na.rm = TRUE))) #maintained 79 columns
 
 '~~~Correlations~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
@@ -58,26 +58,26 @@ STCor <- cor(STData)
 MixedCor <- cor(MixedData)
 INVCor <- cor(INVData) #In cor(INVData) : the standard deviation is zero
 
-'~~~Priminitive Heatmaps~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~' 
+'~~~Priminitive Heatmaps~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 heatmap(AllCor) #<- just using the correlation for all data
 
 AllPhenoScaled <- scale(AllPhenonarrow)
 AllPhenoScaledCor <- cor(AllPhenoScaled)
 Heatmap <- heatmap(AllPhenoScaledCor)
 
-'~~~Modulated Modularity Clustering Heatmaps ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~' 
+'~~~Modulated Modularity Clustering Heatmaps ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 library("pheatmap")
 library("dendsort")
 
 pheatmap(AllCor,fontsize = 3) #<- using non-scaled data (CORRECT)
 
-pheatmap(AllCor,fontsize = 3, kmeans_k = 30) #<- interesting, provides a customizeable horizontal number of vertices 
+pheatmap(AllCor,fontsize = 3, kmeans_k = 30) #<- interesting, provides a customizeable horizontal number of vertices
 
 
 callback = function(hc,...){dendsort(hc)}
 
 pheatmap(AllCor,fontsize = 3, clustering_callback = callback)
-STCorHeatmap <- pheatmap(STCor,fontsize = 2, clustering_callback = callback) 
+STCorHeatmap <- pheatmap(STCor,fontsize = 2, clustering_callback = callback)
 INVCorHeatmap <- pheatmap(INVCor,fontsize = 2, clustering_callback = callback) #<- there are NAs
 
 '~~~Creating Histograms ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
@@ -96,7 +96,7 @@ distINVScaled <- dist(INVScaled)
 ClustINVScaled <- hclust(distINVScaled) # forms a hierarchical cluster of the data points based on distance metric on dataset objects
 plot(ClustINVScaled, ylab = "Height", xlab="Distance")
 
-'~~~Modulated Modularity Clustering - Net ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~' 
+'~~~Modulated Modularity Clustering - Net ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 library("modMax")
 library("igraph")
 
@@ -166,21 +166,21 @@ plot(INVCorCeb, INVCorNet)
 
 '~~~PCA tables~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 ##Creating PCA plots
-##    name               description                          
-## 1  "$eig"             "eigenvalues"                        
-## 2  "$var"             "results for the variables"          
-## 3  "$var$coord"       "coord. for the variables"           
+##    name               description
+## 1  "$eig"             "eigenvalues"
+## 2  "$var"             "results for the variables"
+## 3  "$var$coord"       "coord. for the variables"
 ## 4  "$var$cor"         "correlations variables - dimensions"
-## 5  "$var$cos2"        "cos2 for the variables"             
-## 6  "$var$contrib"     "contributions of the variables"     
-## 7  "$ind"             "results for the individuals"        
-## 8  "$ind$coord"       "coord. for the individuals"         
-## 9  "$ind$cos2"        "cos2 for the individuals"           
-## 10 "$ind$contrib"     "contributions of the individuals"   
-## 11 "$call"            "summary statistics"                 
-## 12 "$call$centre"     "mean of the variables"              
-## 13 "$call$ecart.type" "standard error of the variables"    
-## 14 "$call$row.w"      "weights for the individuals"        
+## 5  "$var$cos2"        "cos2 for the variables"
+## 6  "$var$contrib"     "contributions of the variables"
+## 7  "$ind"             "results for the individuals"
+## 8  "$ind$coord"       "coord. for the individuals"
+## 9  "$ind$cos2"        "cos2 for the individuals"
+## 10 "$ind$contrib"     "contributions of the individuals"
+## 11 "$call"            "summary statistics"
+## 12 "$call$centre"     "mean of the variables"
+## 13 "$call$ecart.type" "standard error of the variables"
+## 14 "$call$row.w"      "weights for the individuals"
 ## 15 "$call$col.w"      "weights for the variables"
 
 library(ggplot2)
@@ -244,21 +244,31 @@ PrincipleComponents2$In2LtStatus <- In2LtStatus
 
 '~~~~~~Intercept and Inversion Status Graphs~~~~~~~~~~~~~~~~~~~~~~~~'
 #incercept and inversions status flux graphs( i = 1:4, 1:10, 1:20, 1:40)
-write_xlsx(PrincipleComponents,"/Users/dgg/Desktop/PrincipleComponents.xlsx") 
+write_xlsx(PrincipleComponents,"/Users/dgg/Desktop/PrincipleComponents.xlsx")
 
-PrincipleComponentsTrimmed <- fread("/Users/dgg/Desktop/PrincipleComponentsWOLastFourRows.csv") #extra 3 rows removed 
+PrincipleComponentsTrimmed <- fread("/Users/dgg/Desktop/PrincipleComponentsWOLastFourRows.csv") #extra 3 rows removed
 MeltedPrincipleComponents <- as.data.table(melt(PrincipleComponentsTrimmed, id.variables = c("RalId","In2LtStatus"), variable.name = "PC", value.name="ComponentValues"))
 
 AttemptV1 <- lm(V1 ~ In2LtStatus, data = PrincipleComponentsTrimmed)
 
 A <- foreach(i=1:20) %do% {
    #i = 1
-   X <- lm(ComponentValues ~ In2LtStatus, data = MeltedPrincipleComponents[PC == paste("V", i, sep = "")]) #intercept = INV data 
-   out = data.frame(
-      PC = paste("PC", i, sep = ""),
-      INV = X$coefficients[1],
-      ST = X$coefficients[3]
-   )
+
+   #X <- lm(ComponentValues ~ In2LtStatus, data = MeltedPrincipleComponents[PC == paste("V", i, sep = "")]) #intercept = INV data
+   #out = data.frame(
+   #    PC = paste("PC", i, sep = ""),
+   #    INV = X$coefficients[1],
+   #    ST = X$coefficients[3]
+   #)
+
+   X <- lm(ComponentValues ~ In2LtStatus - 1, data = MeltedPrincipleComponents[PC == paste("V", i, sep = "")]) ### no intercept model
+   out <- data.table(PC=i,
+                     est=summary(X)$coef[,1],
+                     se=summary(X)$coef[,2],
+                     t=summary(X)$coef[,3],
+                     p=summary(X)$coef[,4],
+                     genotype=c("INV", "HET", "STD")) ### <- This way out structuring the output will make your life easier down the road. The only thing to double check is that the output of the summary(X) table is in the same order as you list here.
+
    return(out)
 }
 
@@ -273,9 +283,9 @@ print(PCAContributionsGraph + ggtitle("PC Coefficients from INV & ST Lines"))
 #graph finding the significance of the hed and std terms
 B <- foreach(i=1:100,.combine = "rbind") %do% {
    #i = 1
-   X <- lm(ComponentValues ~ In2LtStatus, data = MeltedPrincipleComponents[PC == paste("V", i, sep = "")]) #intercept = INV data 
+   X <- lm(ComponentValues ~ In2LtStatus, data = MeltedPrincipleComponents[PC == paste("V", i, sep = "")]) #intercept = INV data
    Y <- summary(X)$coefficients
-   out = data.frame( 
+   out = data.frame(
       t = Y[,3],
       p = Y[,4],
       term = c("Int", "het", "std"),
@@ -286,8 +296,8 @@ B <- foreach(i=1:100,.combine = "rbind") %do% {
 B <- as.data.table(B)
 
 PCASTandINVGraph <- ggplot(B[term != "Int"], aes(x=id, y=-log10(p), group=term, color = term)) +
-   geom_line() + 
-   geom_point() + 
+   geom_line() +
+   geom_point() +
    geom_hline(yintercept = 0.89) + #significance of 0.05060999
    geom_hline(yintercept = 1.12) #significance of -0.04921802
 
@@ -309,11 +319,11 @@ diag(INVCorEx) <- NA #mean = NA
 INVCorSheared <- na.omit(expand.grid(INVCorEx))
 
 #crating a clipped ST dataset:
-INVPheno <- as.vector(colnames(INVDataNoRal)) #77 Phenotypes 
+INVPheno <- as.vector(colnames(INVDataNoRal)) #77 Phenotypes
 STDataMatchedINV <- STData[, ..INVPheno] #eliminates all phenotypes other than those mentioned in INVData
 
 #Using foreach to get hist of ST Bootstrapped recombinations
-STBootstrapINVPheno <- foreach(i=1:100) %do% { 
+STBootstrapINVPheno <- foreach(i=1:100) %do% {
    set.seed(i)
    nLines <- dim(STDataMatchedINV)[1]
    Experiment <- cor(STDataMatchedINV[sample(nLines, 15, replace=F),])
@@ -321,7 +331,7 @@ STBootstrapINVPheno <- foreach(i=1:100) %do% {
    diag(Experiment) <- NA #removes diagonal 1 cor
    out <- data.table(
       expand.grid(Experiment)[,1],
-      bootstrap.id = i,  #all outputs are labeled at bootstrap.id = i #use STBootstrapINVPheno as properly counted output 
+      bootstrap.id = i,  #all outputs are labeled at bootstrap.id = i #use STBootstrapINVPheno as properly counted output
       p1 = rep(dimnames(Experiment)[[1]], (dim(Experiment)[1])),
       p2 = rep(dimnames(Experiment)[[1]], each=(dim(Experiment)[1]))
    )
@@ -330,26 +340,26 @@ STBootstrapINVPheno <- foreach(i=1:100) %do% {
 
 #creating an rbindlist from loop output
 o= rbindlist(STBootstrapINVPheno) #'ral id' column included
-means = o #tiddyverse to find means based on bootstrap groups group(i) command 
+means = o #tiddyverse to find means based on bootstrap groups group(i) command
 
 #Boxplot Comparing Mean Averages Between Bootstraps and INV Data
-means = o %>% 
-   group_by(bootstrap.id) %>% 
-   summarise(Mean = mean(V1)) 
+means = o %>%
+   group_by(bootstrap.id) %>%
+   summarise(Mean = mean(V1))
 mean.observed = mean(INVCorSheared$Var1)
 
 ggplot()  +
-   geom_boxplot(data = (means), aes(bootstrap.id, Mean)) + 
+   geom_boxplot(data = (means), aes(bootstrap.id, Mean)) +
    geom_point(aes(x = 50, y = mean.observed, size=5, col = rgb(0,0,1,0.4))) +
    ggtitle("INV/INV and ST/ST Bootstraps Correlation")
        #Warning message:
-          #Continuous x aesthetic -- did you forget aes(group=...)? 
+          #Continuous x aesthetic -- did you forget aes(group=...)?
 
    #means = as.numeric(means)
-   #means = o %>% 
-      #group_by(bootstrap.id) %>% 
-      #summarise(mean.v1 = mean(o$V1)) %>% 
-      #mutate(bootstrap.id = "bootstrap.id") #dunno what this line did 
+   #means = o %>%
+      #group_by(bootstrap.id) %>%
+      #summarise(mean.v1 = mean(o$V1)) %>%
+      #mutate(bootstrap.id = "bootstrap.id") #dunno what this line did
    #mean.observed = mean(INVCorSheared$Var1)
 
    #ggplot()  +
@@ -365,7 +375,7 @@ ggplot() +
 
 #overlapping the histograms
 hist(INVCorSheared, main = 'distribution of INV values',
-     breaks = seq(-1.0, 1.0, 0.05)) #why isn't this breaking the histogram, or adding the title, as commanded? 
+     breaks = seq(-1.0, 1.0, 0.05)) #why isn't this breaking the histogram, or adding the title, as commanded?
 histST <- lapply(STPermutationsINVPheno, hist, plot = FALSE)
 histINV <- hist(INVCorSheared, plot = FALSE)
 plot (histST, col = rgb(1,0,0,0.4), xlab = 'Correlation',freq = FALSE, main = 'Comparative Histogram')
@@ -378,11 +388,11 @@ histINV <- hist(INVCorSheared, breaks=10)
 STCorStripped <- fread("/Users/dgg/Desktop/STLinesComparisonMatrix.csv")
 INVCorStripped <-fread("/Users/dgg/Desktop/INVLinesComparisonMatrix.csv")
 
-cor(c(as.matrix(STCorStripped)), c(as.matrix(INVCorStripped))) #sums both matrices into vectors, compares those vectors 
-   0.4537692 
+cor(c(as.matrix(STCorStripped)), c(as.matrix(INVCorStripped))) #sums both matrices into vectors, compares those vectors
+   0.4537692
 
-#using Conanical Correlation Analysis (CCA) <- aims to reduce dimensionality similarly to PCA, but for two high dimensional datasets 
-#tries to find directions or projections that account for most of co-variance between two datasets 
+#using Conanical Correlation Analysis (CCA) <- aims to reduce dimensionality similarly to PCA, but for two high dimensional datasets
+#tries to find directions or projections that account for most of co-variance between two datasets
 library(CCA)
 
 cc_results <- cancor(STCorStripped, INVCorStripped)
@@ -394,16 +404,7 @@ cc_results$cor
 attemps <- as.data.frame(STPermutationsINVPheno)
 write_xlsx(attemps,"/Users/dgg/Desktop/experiment1ST.xlsx")
 
-INVCor 
+INVCor
 barchart(INVCor)
 experiment1 <- as.data.frame(INVCor)
-write_xlsx(experiment1,"/Users/dgg/Desktop/experiment1.xlsx") 
-
-   
-   
-   
-   
-   
-   
-   
-
+write_xlsx(experiment1,"/Users/dgg/Desktop/experiment1.xlsx")
